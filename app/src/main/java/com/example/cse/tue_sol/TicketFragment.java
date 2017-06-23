0a2ff70dd7cdc54b9868b0dbf39324b2b8860ad8
SangGeePark <erunningo@gmail.com>
2017-06-23 12:40:13 +0900

    set a php

--- app/src/main/java/com/example/cse/tue_sol/TicketFragment.java
+++ app/src/main/java/com/example/cse/tue_sol/TicketFragment.java
@@ -6,15 +6,38 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by erunn on 2017-06-22.
  */
 
 public class TicketFragment extends Fragment {
+    private WebView webView;
+    final static String url = "http://team4team4.esy.es/data.php";
+    String myUrl;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticket, container, false);
+        webView = (WebView)view.findViewById(R.id.scene_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+
+        if(myUrl == null)
+            myUrl = url;
+        webView.loadUrl(myUrl);
+
+        return view;
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            myUrl = url;
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
