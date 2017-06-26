ef42bc06492c803225ddf52fe3730022efbff192
gyutaebbb <foof2402@gmail.com>
2017-06-26 16:18:44 +0900

    add alert

--- app/src/main/java/com/example/cse/tue_sol/TicketFragment.java
+++ app/src/main/java/com/example/cse/tue_sol/TicketFragment.java
@@ -1,10 +1,13 @@
 package com.example.cse.tue_sol;
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,33 +16,73 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
+import java.util.HashMap;
+
 /**
  * Created by erunn on 2017-06-22.
  */
 
-public class TicketFragment extends Fragment implements View.OnClickListener{
+public class TicketFragment extends Fragment implements View.OnClickListener {
 
     private View view;
     private WebView webView;
     final static String url = "http://team4team4.esy.es/data.php";
     private Button button1, button2;
+    private SQLiteHandler db;
+    private SessionManager session;
+    private SQLiteDatabase sqlDB;
 
     String myUrl;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_ticket, container, false);
-        webView = (WebView)view.findViewById(R.id.scene_view);
+        webView = (WebView) view.findViewById(R.id.scene_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new MyWebViewClient());
+        // SqLite database handler
+        db = new SQLiteHandler(getActivity());
 
+        // session manager
 
-        if(myUrl == null)
-            myUrl = url;
+        // Fetching user details from SQLite
+        HashMap<String, String> user = db.getUserDetails();
+        String email = user.get("email");
+        String name = user.get("name");
+        if (TextUtils.isEmpty(email)) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+
+            // ????
+            alertDialogBuilder.setTitle("Korail talk+");
+
+            // AlertDialog ??
+
+            alertDialogBuilder
+                    .setMessage("로그인이 필요합니다.")
+                    .setCancelable(false)
+                    .setPositiveButton("확인",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(
+                                        DialogInterface dialog, int id) {
+                                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                                    startActivity(intent);
+                                    getActivity().finish();
+
+                                }
+                            });
+            // ????? ??
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+        String PostUrl = url + "?email=" + email;
+        Log.d("Url is", PostUrl);
+        if (myUrl == null)
+            myUrl = PostUrl;
         webView.loadUrl(myUrl);
-        button1=(Button)view.findViewById(R.id.bottom_home);
+        button1 = (Button) view.findViewById(R.id.bottom_home);
         button1.setOnClickListener(this);
-        button2 = (Button)view.findViewById(R.id.bottom_search);
+        button2 = (Button) view.findViewById(R.id.bottom_search);
         button2.setOnClickListener(this);
 
 
@@ -48,7 +91,7 @@ public class TicketFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-        Log.v("aaaaa","oncLick");
+        Log.v("aaaaa", "oncLick");
         Intent intent = null;
         switch (view.getId()) {
             case R.id.bottom_home:
@@ -59,8 +102,8 @@ public class TicketFragment extends Fragment implements View.OnClickListener{
             default:
                 break;
         }
-        if(intent != null)
-         startActivity(intent);
+        if (intent != null)
+            startActivity(intent);
         getActivity().finish();
 
     }
